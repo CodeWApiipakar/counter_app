f94f6d83e7cee5b3e9171e68fc9e7df0b92e2df7
CodeWApiipakar <abuubakarmoha2023@gmail.com>
2024-08-25 22:41:20 +0300

    added huge updates

---
+++ lib/components/items.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+Widget itemsList(screenSize, items, getContainerSize) {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+    decoration: BoxDecoration(),
+    width: screenSize.width,
+    height: screenSize.height,
+    child: ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        String key = items.keys.elementAt(index);
+        int colorValue = items[key]![1];
+        String itemValue = items[key]![0];
+
+        print(items.length);
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          // padding: EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+              color: Color(colorValue).withOpacity(0.9),
+              borderRadius: BorderRadius.all(Radius.circular(4))),
+          child: Column(
+            children: [
+              Container(
+                color: Color(colorValue).withOpacity(0.75),
+                padding: EdgeInsets.all(0),
+                margin: EdgeInsets.symmetric(horizontal: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      key,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.edit,
+                        ))
+                  ],
+                ),
+              ),
+              Container(
+                height: getContainerSize,
+                child: Center(
+                    child: Text(
+                  itemValue,
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                )),
+              )
+            ],
+          ),
+        );
+      },
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:counter/components/dialog.dart';
+import 'package:counter/components/items.dart';
 import 'package:counter/components/myAppBar.dart';
 import 'package:counter/screens/splashScreenOne.dart';
 import 'package:flutter/material.dart';
@@ -33,35 +34,30 @@ class _HomeState extends State<Home> {
   int? itemValue;
   String itemTitle = "Create new item";
 
-// change value items and save them to shared prefs
   void handleChanges(String newItemId, String itemValue, var itemColor) async {
     // Retrieve SharedPreferences instance
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
     // Perform the async operation outside of setState
-    Map<String, List<dynamic>> updatedItems =
+    Map<String, List<dynamic>> existingItems =
         await getItemsFromSharedPreferences();
 
-    // Update itemid and items map
-    setState(() {
-      if (newItemId.isNotEmpty) {
-        // Check if the newItemId exists
-        if (updatedItems.containsKey(newItemId)) {
-          // Update existing entry
-          updatedItems[newItemId] = [itemValue, itemColor];
-        } else {
-          // Add new entry
-          updatedItems[newItemId] = [itemValue, itemColor];
-        }
-
-        // Optionally update itemid if needed
-        itemid = newItemId;
-      }
+    // Check if the newItemId is not empty and doesn't already exist in the map
+    if (newItemId.isNotEmpty && !existingItems.containsKey(newItemId)) {
+      // Add the new item to the map
+      existingItems[newItemId] = [itemValue, itemColor];
 
       // Convert updated items map to JSON string and save it to SharedPreferences
-      String jsonItems = jsonEncode(updatedItems);
-      prefs.setString("items", jsonItems);
-    });
+      String jsonItems = jsonEncode(existingItems);
+      await prefs.setString("items", jsonItems);
+
+      // Optionally update itemid if needed inside setState
+      setState(() {
+        itemid = newItemId;
+      });
+
+      getOldItems();
+    }
   }
 
 // Function to get items from SharedPreferences
@@ -69,7 +65,7 @@ class _HomeState extends State<Home> {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     String? jsonString = prefs.getString('items');
     if (jsonString != null) {
-      Map<String, List<dynamic>> map = jsonDecode(jsonString);
+      Map<String, dynamic> map = jsonDecode(jsonString);
       return map.map((key, value) => MapEntry(key, List<dynamic>.from(value)));
     }
     return {};
@@ -103,8 +99,8 @@ class _HomeState extends State<Home> {
       setState(() {
         items = jsonObject;
       });
-      print(items);
     }
+    print(items);
   }
 
   void removeOldItems() async {
@@ -112,12 +108,32 @@ class _HomeState extends State<Home> {
     prefs.remove("items");
   }
 
+//container size handler.
+  double getContainerSize(itemsLength) {
+    double size;
+    if (itemsLength == 2) {
+      size = 200;
+    } else if (itemsLength == 3) {
+      size = 160;
+    } else if (itemsLength == 4) {
+      size = 110;
+    } else {
+      size = 80;
+    }
+    return size;
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: buildAppBar(addNewItem),
-      body: Center(),
+      body: items.isEmpty
+          ? Center(
+              child: Text("Click + icon to create items"),
+            )
+          : itemsList(screenSize, items, getContainerSize(items.length)),
     );
   }
 }
